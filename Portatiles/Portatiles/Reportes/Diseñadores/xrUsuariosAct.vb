3d20b3e875dad82b591a521c6a046df8850dbbb5
arieldavg <aridg_rm87@yahoo.es>
2017-06-03 11:57:03 -0600

    AADG - Commit 03/06/2017
    Mas pruebas de reportes

--- Portatiles/Portatiles/Reportes/Diseñadores/xrUsuariosAct.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class xrUsuariosAct
-
-End Class
--- Portatiles/Portatiles/Reportes/xrUsuarios.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class xrUsuarios
-
-    Private Sub xrUsuarios_DesignerLoaded(sender As Object, e As DevExpress.XtraReports.UserDesigner.DesignerLoadedEventArgs) Handles MyBase.DesignerLoaded
-
-    End Sub
-End Class
